fda09a16730756793bcbf833facd6f8c6787ea37
Yusuf Numan YILMAZ <ysfnmnylmz@gmail.com>
2020-07-21 23:24:10 +0300

    Updates

---
+++ precache-manifest.c71a40def1d0426687d6d1b028cb65b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77def2503958e7ab75302008d7b77be3",
+    "url": "/omdbapi/index.html"
+  },
+  {
+    "revision": "a5183fa4b5ceec5f8f23",
+    "url": "/omdbapi/static/css/main.019de4e7.chunk.css"
+  },
+  {
+    "revision": "d6638e1e04f236608977",
+    "url": "/omdbapi/static/js/2.4ac50568.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/omdbapi/static/js/2.4ac50568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5183fa4b5ceec5f8f23",
+    "url": "/omdbapi/static/js/main.3245ad8f.chunk.js"
+  },
+  {
+    "revision": "b7863c2b7c330c306257",
+    "url": "/omdbapi/static/js/runtime-main.3e209b94.js"
+  },
+  {
+    "revision": "990a45c6c4f20dd0fd88a9dcaf2683f6",
+    "url": "/omdbapi/static/media/no-poster.990a45c6.jpg"
+  }
+]);
